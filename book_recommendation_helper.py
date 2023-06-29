99a5f8263ae2dbfb9afca1d486984b090f8ed5d2
Daayam <daayam.sial@gmail.com>
2023-06-29 14:57:10 +0100

    The function now returns a list of books by catagory

--- book_recommendation_helper.py
+++ book_recommendation_helper.py
@@ -5,3 +5,10 @@ def sort_by_catagory():
     books_by_catagory = {}
     for catagory in book_catagories:
         books_by_catagory[catagory] = []
+        for book in books_copy:
+            if book[1][1] == catagory:
+                books_by_catagory[catagory].append(book[0])
+                books_copy.remove(book)
+    return books_by_catagory.items()
+
+print(sort_by_catagory())
