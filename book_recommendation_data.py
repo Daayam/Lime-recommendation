a80efc8dd2287241144cfbb8b28b0a8d8eae9859
Daayam <daayam.sial@gmail.com>
2023-06-29 10:57:03 +0100

    Created a book dictionary and added 10 books to it

--- book_recommendation_data.py
+++ book_recommendation_data.py
@@ -1,2 +1,13 @@
 
 book_catagories = ["Biographies & Memoirs", "Classics", "Health & Fitness", "History", "Money & Finance", "Self Help"]
+
+books = {"Atomic Habits": ["James Clear", "Self Help",""], 
+"The Four Agreements":["", "", ""], 
+"Rich Dad Poor Dad": ["Robert Kiyosaki", "Money & Finance", ""], 
+"Think and Grow Rich": ["Napoleon Hill", "Money & Finance", ""],
+"The Richest Man in Babylon": ["George S. Clason", "Money & Finance", ""],
+"How to Win Friends and Influence People": ["Dale Carnegie", "Self Help", ""],
+"You are a Badass": ["Jen Sincero", "Self Help", ""],
+"Deep Work": ["Cal Newport", "Self Help", ""],
+"Never Split the Difference": ["Chriss Voss", "Money & Finance", ""],
+"The 4-hour Workweek": ["Timothy Ferris", "Money & Finance", ""]}
