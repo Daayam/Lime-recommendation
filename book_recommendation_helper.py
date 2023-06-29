af714e6e0f6466b355fa820fabf78e4e1226ef24
Daayam <daayam.sial@gmail.com>
2023-06-29 15:47:09 +0100

    Sort by catagory function was omitting certain entries. Fixed it and simplified function using list comprehension

--- book_recommendation_helper.py
+++ book_recommendation_helper.py
@@ -3,12 +3,10 @@ from book_recommendation_data import book_catagories, books
 def sort_by_catagory():
     books_copy = [ [title, attributes] for title, attributes in books.items()]
     books_by_catagory = {}
-    for catagory in book_catagories:
-        books_by_catagory[catagory] = []
-        for book in books_copy:
-            if book[1][1] == catagory:
-                books_by_catagory[catagory].append(book[0])
-                books_copy.remove(book)
+    
+    for catagory in book_catagories:        
+        books_by_catagory[catagory] = [book[0] for book in books_copy if book[1][1] == catagory]
+        
     return books_by_catagory.items()
 
 print(sort_by_catagory())
