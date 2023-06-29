cb407e69eadaecbe8bb7fb211f1cad3486ddeee5
Daayam <daayam.sial@gmail.com>
2023-06-29 16:01:49 +0100

    Sort by catagory now returns a list of lists instead of a list of tuples

--- book_recommendation_helper.py
+++ book_recommendation_helper.py
@@ -3,10 +3,10 @@ from book_recommendation_data import book_catagories, books
 def sort_by_catagory():
     books_copy = [ [title, attributes] for title, attributes in books.items()]
     books_by_catagory = {}
-    
+
     for catagory in book_catagories:        
         books_by_catagory[catagory] = [book[0] for book in books_copy if book[1][1] == catagory]
-        
-    return books_by_catagory.items()
+    sorted_list = [[catagory, title] for catagory, title in books_by_catagory.items()]
+    return sorted_list
 
 print(sort_by_catagory())
