fe61f1ea2b5bb8ccef708c9b9ff0b55ffb618aed
Daayam <daayam.sial@gmail.com>
2023-06-30 23:37:38 +0100

    Main file: created function to take input from user for catagory search

--- book_recommendation.py
+++ book_recommendation.py
@@ -2,3 +2,7 @@ from book_recommendation_helper import sort_by_catagory, print_list, quick_sort
 
 def inro():
     print("Welcome to the greatest book recommendation software! *")
+
+def what_catagory():
+    print("Which Genre are you looking for?")
+    choice = input("Please enter the first letter or first few letters of the Genre you are looking for and we'll let you know if we have books in that catagory ")
